4ad77c70989216895fa0f7762f35a61a59f7ef10
= <igomorev@bk.ru>
2022-08-18 18:31:10 +0300

    Добавлен возраст

--- src/For_human_one.java
+++ src/For_human_one.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class For_human_one {
+public class For_human_one {
+    static String human(int age){
+        System.out.println("My name is Maria");
+        int yearsToRetiretment = 65 - age;
+        return Integer.toString(yearsToRetiretment);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        int age = Integer.parseInt(string);
+        For_human_one.human(age);
+        String s = For_human_one.human(age);
+        System.out.println(s);
     }
 }
