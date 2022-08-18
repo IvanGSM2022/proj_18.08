3667b4498209be3362396c48ee214cfc1f07db41
= <igomorev@bk.ru>
2022-08-18 18:18:22 +0300

    Initial commit

---
+++ For_human_two.java
@@ -0,0 +1,7 @@
+public class For_human_two {
+    static int brother(int age){
+        int yearsToRetirement = 65 - age;
+        return yearsToRetirement;
+        String name = "Pavel";
+    }
+}
---
+++ src/For_human_one.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class For_human_one {
+}
